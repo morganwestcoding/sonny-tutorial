1d6c00c8991d2c0303d2876d39f8bc0c5d01c0c9
Morgan West <mewwes@gmail.com>
2023-05-25 19:57:55 -0700

    Counter setup

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
 
 function App() {
+  const [count, setCount] = useState(0);
+
+  const increment = () => {
+
+    setCount(count + 1) //CORRECT
+  }
+
+  const decrement = () => {
+
+    setCount(count - 1) //CORRECT
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Welcome to my counter app</h1>
+      <p>The count is {count}</p>
+      <button onClick={increment}>-</button>
+      <button onClick={decrement}>+</button>
     </div>
   );
+
 }
+
 
 export default App;
---
+++ src/Header.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Header() {
+  return (
+    <div className='header'>
+      <h1 className="header__title">This is the Header</h1>
+      <h2 className="header__user">Sonny is signed in</h2>
+    </div>
+  )
+}
+
+export default Header
