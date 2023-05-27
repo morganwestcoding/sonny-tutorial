e971056edff2f7c1ab2fd0e2d76b7f92f2c07cf2
Morgan West <mewwes@gmail.com>
2023-05-27 11:41:56 -0700

    todo map button

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [todos, setTodos] = useState([
+    "Take the dogs out for a walk",
+    "Take the trash out",
+  ]);
   return (
     <div className='app'>
-      <h1>Hello World</h1>
+      <h1>Welcome to my todo list</h1>
+      <input type='text'/>
+      <button>Add todo</button>
+
+      <h2>List the Todos</h2>
+      {todos.map((todo) => (
+        <p>{todo}</p>
+      ))}
     </div>
   );
 }
