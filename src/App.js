923478f89377468612a9bf9fcd4c940745ccb51e
Morgan West <mewwes@gmail.com>
2023-05-27 12:24:29 -0700

    refresh corrected

--- src/App.js
+++ src/App.js
@@ -6,17 +6,24 @@ function App() {
   const [input, setInput] = useState("");
 
   const addTodo = e => {
+    e.preventDefault();
+    
     console.log(`this is the input ${input}`);
     setTodos([...todos, input])
+    setInput("");
   };
 
   return (
     <div className='app'>
       <h1>Welcome to my todo list</h1>
-      <input value={input} onChange={e => setInput(e.target.value)} type='text'/>
 
-      <button onClick={addTodo}>Add todo</button>
+      <form>
+        <input value={input} onChange={e => setInput(e.target.value)} type='text'/>
 
+        <button type="submit" onClick={addTodo}>Add todo</button>
+
+      </form>
+     
       <h2>List the Todos</h2>
       {todos.map((todo) => (
         <p>{todo}</p>
