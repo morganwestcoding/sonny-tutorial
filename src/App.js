dd6ba89c5cb7a2ad4e22efadfd7b7a1fe6c8ebc1
Morgan West <mewwes@gmail.com>
2023-05-26 23:45:43 -0700

    rework

--- src/App.js
+++ src/App.js
@@ -1,28 +1,12 @@
-import React, { useState } from 'react';
+import React from 'react';
+import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0);
-
-  const increment = () => {
-
-    setCount(count + 1) //CORRECT
-  }
-
-  const decrement = () => {
-
-    setCount(count - 1) //CORRECT
-  }
-
   return (
-    <div>
-      <h1>Welcome to my counter app</h1>
-      <p>The count is {count}</p>
-      <button onClick={decrement}>-</button>
-      <button onClick={increment}>+</button>
+    <div className='app'>
+      <h1>Hello World</h1>
     </div>
   );
-
 }
 
-
-export default App;
+export default App
---
+++ src/ItemDescription.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+
+export default class ItemDescription extends Component {
+  render() {
+    const { name, description} = this.props;
+    return (
+      <div>
+        <p>{name}</p>
+        <p>
+            <i>{description}</i>
+        </p>
+      </div>
+    );
+  }
+}
