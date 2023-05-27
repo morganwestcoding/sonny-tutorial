d5fb0ff4b3c96aa2329bfa01ee02442155c70fe8
Morgan West <mewwes@gmail.com>
2023-05-27 12:16:19 -0700

    input save

--- src/App.js
+++ src/App.js
@@ -2,15 +2,20 @@ import React, { useState } from 'react';
 import './App.css';
 
 function App() {
-  const [todos, setTodos] = useState([
-    "Take the dogs out for a walk",
-    "Take the trash out",
-  ]);
+  const [todos, setTodos] = useState([]);
+  const [input, setInput] = useState("");
+
+  const addTodo = e => {
+    console.log(`this is the input ${input}`);
+    setTodos([...todos, input])
+  };
+
   return (
     <div className='app'>
       <h1>Welcome to my todo list</h1>
-      <input type='text'/>
-      <button>Add todo</button>
+      <input value={input} onChange={e => setInput(e.target.value)} type='text'/>
+
+      <button onClick={addTodo}>Add todo</button>
 
       <h2>List the Todos</h2>
       {todos.map((todo) => (
